5d5866f67d09db56438e574d8f1850f4afc109a7
Aditya <aditya.gup16@gmail.com>
2023-11-15 22:05:31 -0700

    -Move variables to class from method in Graphics.java

--- src/Graphics.java
+++ src/Graphics.java
@@ -14,52 +14,61 @@ import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 
 public class Graphics {
+    private static JFrame frame;
+    private static JPanel mainPanel;
+    private static JPanel startingCurrencyButtonsPanel;
+    private static ArrayList<GeneralCurrency> currencies;
+    private static ButtonGroup startingCurrencyButtons;
+    private static JPanel textFieldPanel;
     private static JTextField amountField;
+    private static JPanel endingCurrencyButtonsPanel;
+    private static ButtonGroup endingCurrencyButtons;
+    private static JPanel convertButtonPanel;
+    private static JButton convertButton;
 
     public Graphics() {
     }
 
     public static void createPage() {
-        JFrame frame = new JFrame("Currency Converter");
+        frame = new JFrame("Currency Converter");
         frame.setDefaultCloseOperation(3);
-        JPanel mainPanel = new JPanel();
+        mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, 1));
-        JPanel startingCurrencyButtonsPanel = new JPanel();
-        ArrayList<GeneralCurrency> currencies = Currency.getCurrencyList();
-        ButtonGroup startingCurrencyButtons = new ButtonGroup();
-        Iterator var5 = currencies.iterator();
+        startingCurrencyButtonsPanel = new JPanel();
+        currencies = Currency.getCurrencyList();
+        startingCurrencyButtons = new ButtonGroup();
 
-        while(var5.hasNext()) {
-            GeneralCurrency currency = (GeneralCurrency)var5.next();
+        for (GeneralCurrency currency : currencies) {
             JToggleButton toggleButton = new JToggleButton(currency.getName());
             startingCurrencyButtonsPanel.add(toggleButton);
             startingCurrencyButtons.add(toggleButton);
         }
 
-        JPanel textFieldPanel = new JPanel();
+        textFieldPanel = new JPanel();
         amountField = new JTextField(10);
         textFieldPanel.add(amountField);
-        JPanel endingCurrencyButtonsPanel = new JPanel();
-        ButtonGroup endingCurrencyButtons = new ButtonGroup();
-        Iterator var8 = currencies.iterator();
 
-        while(var8.hasNext()) {
-            GeneralCurrency currency = (GeneralCurrency)var8.next();
+        endingCurrencyButtonsPanel = new JPanel();
+        endingCurrencyButtons = new ButtonGroup();
+
+        for (GeneralCurrency currency : currencies) {
             JToggleButton toggleButton = new JToggleButton(currency.getName());
             endingCurrencyButtonsPanel.add(toggleButton);
             endingCurrencyButtons.add(toggleButton);
         }
 
-        JPanel convertButtonPanel = new JPanel();
-        JButton convertButton = new JButton("Convert!");
+        convertButtonPanel = new JPanel();
+        convertButton = new JButton("Convert!");
         convertButton.addActionListener((e) -> {
             System.out.println("Converting!");
         });
         convertButtonPanel.add(convertButton);
+
         mainPanel.add(startingCurrencyButtonsPanel);
         mainPanel.add(textFieldPanel);
         mainPanel.add(endingCurrencyButtonsPanel);
         mainPanel.add(convertButtonPanel);
+
         frame.getContentPane().add(mainPanel);
         frame.setSize(1200, 600);
         frame.setVisible(true);
